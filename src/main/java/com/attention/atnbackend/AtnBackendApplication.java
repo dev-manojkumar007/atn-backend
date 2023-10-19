4a2974029fa294af2225b5d541661e8097673554
Manoj Kumar Panchal <manojkumarpanchal@Manojs-MacBook-Air.local>
2023-10-19 23:39:05 +0530

    Initial commit

---
+++ src/main/java/com/attention/atnbackend/AtnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.attention.atnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtnBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attention/atnbackend/AtnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attention.atnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
